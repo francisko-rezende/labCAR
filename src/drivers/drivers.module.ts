60fa6bf8fa92d21155bde49862d910688ed7eef1
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-11-02 22:09:43 -0300

    feat: inject database into drivers.module

--- src/drivers/drivers.module.ts
+++ src/drivers/drivers.module.ts
@@ -1,9 +1,10 @@
+import { Database } from 'src/database/database';
 import { Module } from '@nestjs/common';
 import { DriversController } from './drivers.controller';
 import { DriversService } from './drivers.service';
 
 @Module({
   controllers: [DriversController],
-  providers: [DriversService],
+  providers: [DriversService, Database],
 })
 export class DriversModule {}
