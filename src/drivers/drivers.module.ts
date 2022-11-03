b98e4e478aa50dd6fecf42b3c97fe9c170bc2b18
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-11-03 08:40:42 -0300

    feat: inject is18YearsOld validator into drivers module

--- src/drivers/drivers.module.ts
+++ src/drivers/drivers.module.ts
@@ -2,9 +2,10 @@ import { Database } from 'src/database/database';
 import { Module } from '@nestjs/common';
 import { DriversController } from './drivers.controller';
 import { DriversService } from './drivers.service';
+import { Is18YearsOldConstraint } from 'src/utils/is18YearsOld.validator';
 
 @Module({
   controllers: [DriversController],
-  providers: [DriversService, Database],
+  providers: [DriversService, Database, Is18YearsOldConstraint],
 })
 export class DriversModule {}
