75786f448741a5e3f8fddaac91d3262fa03accb7
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-11-02 22:11:15 -0300

    feat: create initial version of endpoint for registering new drivers

--- src/drivers/drivers.controller.ts
+++ src/drivers/drivers.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Database } from 'src/database/database';
+import { Driver } from './driver.entity';
 
 @Controller('drivers')
 export class DriversController {
+  constructor(private database: Database) {}
+
   @Get()
   findAll() {
     console.log('All drivers');
+  }
+
+  @Post()
+  createDriver(@Body() driver: Driver) {
+    this.database.saveDriver(driver);
   }
 }
