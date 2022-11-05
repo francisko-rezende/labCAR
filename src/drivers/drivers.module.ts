5994629f11a7840d3df1035466962d49d9dd5a87
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-11-05 13:27:59 -0300

    feat: inject fresh cpf decorator

--- src/drivers/drivers.module.ts
+++ src/drivers/drivers.module.ts
@@ -1,3 +1,4 @@
+import { CpfHasntBeenUsedConstraint } from 'src/commons/decorators/hasCpfBeenUsedBefore.validator';
 import { StringUtils } from 'src/utils/stringUtils';
 import { Database } from 'src/database/database';
 import { Module } from '@nestjs/common';
@@ -7,6 +8,12 @@ import { Is18YearsOldConstraint } from 'src/commons/decorators/is18YearsOld.vali
 
 @Module({
   controllers: [DriversController],
-  providers: [DriversService, Database, Is18YearsOldConstraint, StringUtils],
+  providers: [
+    DriversService,
+    Is18YearsOldConstraint,
+    StringUtils,
+    Database,
+    CpfHasntBeenUsedConstraint,
+  ],
 })
 export class DriversModule {}
