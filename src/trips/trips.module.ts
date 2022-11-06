7e1552ea6fe11f6bfa3aa3adccad680b23739567
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-11-06 19:08:21 -0300

    feat: inject database and stringUtils dependencies in trip module

--- src/trips/trips.module.ts
+++ src/trips/trips.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TripsService } from './trips.service';
 import { TripsController } from './trips.controller';
+import { Database } from 'src/database/database';
+import { StringUtils } from 'src/utils/stringUtils';
 
 @Module({
   controllers: [TripsController],
-  providers: [TripsService],
+  providers: [TripsService, Database, StringUtils],
 })
 export class TripsModule {}
