528383344d7ef2587a69273103f51711351a316c
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-11-06 18:32:28 -0300

    feat: create db methods for managing trips

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,11 +2,13 @@ import { Driver } from 'src/drivers/driver.entity';
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
 import { Rider } from 'src/riders/riders.entity';
+import { Trip } from 'src/trips/trip.entity';
 
 @Injectable()
 export class Database {
   private DRIVERS = 'drivers.json';
   private RIDERS = 'riders.json';
+  private TRIPS = 'trips.json';
 
   public getDrivers() {
     const driversInFile = fs.readFileSync(this.DRIVERS).toString();
@@ -40,5 +42,22 @@ export class Database {
     const riders = this.findAllRiders();
     const updatedRiders = [...riders, rider];
     this.createRiders(updatedRiders);
+  }
+
+  public findAllTrips() {
+    const tripsInFile = fs.readFileSync(this.TRIPS).toString();
+    const trips: Trip[] = JSON.parse(tripsInFile);
+    return trips;
+  }
+
+  public createTrips(trips: Trip[]) {
+    const stringfiedTrips = JSON.stringify(trips);
+    fs.writeFileSync(this.TRIPS, stringfiedTrips);
+  }
+
+  public createTrip(trip: Trip) {
+    const trips = this.findAllTrips();
+    const updatedTrips = [...trips, trip];
+    this.createTrips(updatedTrips);
   }
 }
